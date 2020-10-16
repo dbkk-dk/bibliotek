282bb0a3042e9aa0594b273f982fb3df9c574968
paw <pawsen@gmail.com>
2020-10-16 13:24:21 +0200

    Opdater beskrivelse af filen og dens funktioner

--- calc_isbn13.py
+++ calc_isbn13.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
-from bookdb import (
-    insert_book,
-    sanitize_metadata,
-    create_connection,
-    book_exist,
-    updatedb,
-    get_locations_id,
-)
-from isbnlib import is_isbn10, to_isbn13, to_isbn10, notisbn
+
+"""This script polulates the isbn fields in the DB, if some are missing.
+
+The DB have the fields:
+ | isbn | isbn_10 | isbn_13 |
+
+isbn is read from old .mdb DB and inserted if verified(ie correct isbn). isbn_10
+and isbn_13 are populated from the info from openlibrary and google-books.
+
+If isbn_10 is missing and isbn is present, copy from isbn. If isbn_13 is missing
+and isbn_10 present, calculate isbn_13. isbn will always be the original from
+the .mdb DB.
+
+"""
+
+import argparse
 import logging
+
+from isbnlib import notisbn, to_isbn13
+
+from bookdb import create_connection
 
 logging.basicConfig(level=logging.DEBUG)
 # set the root logger to debug. All other loggers ends here, due to chaining
@@ -19,18 +30,12 @@ root.setLevel(logging.DEBUG)
 
 BOOKS_DB = "books.sqlite"
 conn = create_connection(BOOKS_DB)
-id_loc_map = get_locations_id(conn)
-
-"""Remember there can be multiple isbns in the isbn_10 field, due to errors in
-the openlibrary.org db. Thus we must split on ';' and calculate isbn13 for all
-of the them
-
-"""
 
 
 def set_isbn10():
-    sql = 'select id, isbn from book where isbn_10 = ? and isbn != ?;'
-    key = ('', '')
+    """if isbn10 is empty, copy from from isbn"""
+    sql = "select id, isbn from book where isbn_10 = ? and isbn != ?;"
+    key = ("", "")
     cur = conn.cursor()
     cur.execute(sql, key)
     res = cur.fetchall()
@@ -39,7 +44,7 @@ def set_isbn10():
     for book in res:
         id, isbn = book
         if notisbn(isbn):
-            print(f'error, id {id}, isbn {isbn} is not a valid isbn')
+            print(f"error, id {id}, isbn {isbn} is not a valid isbn")
             continue
         isbn_10 = isbn
 
@@ -49,9 +54,16 @@ def set_isbn10():
     cur.executemany(sql, data)
     conn.commit()
 
-try:
-    sql = 'select id, isbn_10 from book where isbn_13 = ? and isbn_10 != ?;'
-    key = ('', '')
+
+def set_isbn13():
+    """If isbn13 is empty, calculate it from isbn10
+
+    Remember there can be multiple isbns in the isbn_10 field, due to errors in
+    the openlibrary.org db. Thus we must split on ';' and calculate isbn13 for
+    all of the them"""
+
+    sql = "select id, isbn_10 from book where isbn_13 = ? and isbn_10 != ?;"
+    key = ("", "")
     cur = conn.cursor()
     cur.execute(sql, key)
     res = cur.fetchall()
@@ -59,11 +71,11 @@ try:
     data = []
     for book in res:
         id, isbns = book
-        isbns = isbns.split('; ')
+        isbns = isbns.split("; ")
         isbn_13 = []
         for isbn_10 in isbns:
             if notisbn(isbn_10):
-                print(f'error, id {id}, isbn_10 {isbn_10} is not a valid isbn')
+                print(f"error, id {id}, isbn_10 {isbn_10} is not a valid isbn")
                 continue
             isbn_13.append(to_isbn13(isbn_10))
         data.append(("; ".join(isbn_13), id))
@@ -72,5 +84,29 @@ try:
     cur.executemany(sql, data)
     conn.commit()
 
-except:
-    pass
+
+def main():
+    # If no arg is given, run the default which is to show the usage
+    # https://stackoverflow.com/a/40613995
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(func=lambda x: parser.print_usage())
+    parser.add_argument(
+        # https://docs.python.org/3/library/argparse.html#action
+        "--isbn10",
+        required=False,
+        action="store_true",  # set to True if present
+    )
+    parser.add_argument("--isbn13", required=False, action="store_true")
+    args = parser.parse_args()
+    args.func(args)
+    try:
+        if args.isbn10:
+            set_isbn10()
+        if args.isbn13:
+            set_isbn13()
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    main()
